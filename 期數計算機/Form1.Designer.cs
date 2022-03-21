d014e406dea55640856104ae7216e5118602c9f4
qq96932100 <qq9693210000@gmail.com>
2022-03-21 15:44:12 +0800

    期數計算機
    透過dateTimePicker計算日期，可計算目標日期為第幾期。

---
+++ 期數計算機/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace 期數計算機
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(238, 19);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(224, 40);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "請選擇起始時間：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(10, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 27);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "請選擇目標時間：";
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker2.Location = new System.Drawing.Point(238, 89);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(224, 40);
+            this.dateTimePicker2.TabIndex = 3;
+            this.dateTimePicker2.ValueChanged += new System.EventHandler(this.dateTimePicker2_ValueChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("標楷體", 18F);
+            this.button1.Location = new System.Drawing.Point(480, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 128);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "開始計算!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 27);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "第0期";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 177);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(222, 27);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "距離目標日期0年";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 214);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(222, 27);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "距離目標日期0月";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(233, 61);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(110, 27);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "民國x年";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(233, 132);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(110, 27);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "民國x年";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(240, 177);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(222, 27);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "距離目標日期0月";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("標楷體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(240, 214);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(222, 27);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "距離目標日期0月";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 263);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dateTimePicker2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "期數計算機";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ 期數計算機/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 期數計算機
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label6.Text = String.Format("民國{0}年", (dateTimePicker1.Value.Year - 1911));
+            label7.Text = String.Format("民國{0}年", (dateTimePicker2.Value.Year - 1911));
+            label8.Text = String.Format("下一期時間為{0}年{1}月{2}日", 0, 0, 0);
+            label9.Text = String.Format("也就是民國{0}年{1}月{2}日", 0, 0, 0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DateTime dt1 = dateTimePicker1.Value;
+            DateTime dt2 = dateTimePicker2.Value;
+            //TimeSpan Diff_dates = dt2.Subtract(dt1);
+            int year = dt2.Year - dt1.Year;
+            int month = dt2.Month - dt1.Month;
+            //label3.Text = String.Format("第{0}期", month);
+            month += year * 12;
+            if(dt2.Day < dt1.Day)
+            {
+                label8.Text = String.Format("下一期時間為{0}年{1}月{2}日",dt2.Year, dt2.Month, dt1.Day);
+                label9.Text = String.Format("也就是民國{0}年{1}月{2}日", dt2.Year-1911, dt2.Month, dt1.Day);
+                --month;
+            }
+            else
+            {
+                label8.Text = String.Format("下一期時間為{0}年{1}月{2}日", dt2.Year, dt2.Month+1, dt1.Day);
+                label9.Text = String.Format("也就是民國{0}年{1}月{2}日", dt2.Year - 1911, dt2.Month+1, dt1.Day);
+            }
+            label3.Text = String.Format("第{0}期",month);
+            label4.Text = String.Format("距離目標日期{0}年", year);
+            label5.Text = String.Format("距離目標日期{0}月", month);
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            label6.Text = String.Format("民國{0}年", (dateTimePicker1.Value.Year - 1911));
+        }
+
+        private void dateTimePicker2_ValueChanged(object sender, EventArgs e)
+        {
+            label7.Text = String.Format("民國{0}年", (dateTimePicker2.Value.Year - 1911));
+        }
+    }
+}
